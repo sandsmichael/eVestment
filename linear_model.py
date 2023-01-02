c2d4125c4e8c8df38c7c623dd88019173ad2c3d5
Michael Sands <michaelesands@gmail.com>
2023-01-02 02:11:55 -0500

    rmse

--- linear_model.py
+++ linear_model.py
@@ -33,24 +33,22 @@ for c in df.columns:
     except:
         pass
 
-# print(df.head())
-# print(df.columns)
-
-
 returns_columns = [ c for c in df.columns if 'Returns - ' in c and 'in' not in c ][-36:] # Last 36 return columns are individual monthly series'
 monthly_returns = df[returns_columns].T.iloc[::-1]
 monthly_returns.index = [c.rsplit(' - ')[-1].replace(')', '') for c in monthly_returns.index]
-print(monthly_returns)
-# print(monthly_returns.columns.tolist())
 
 
 product_name = 'AGF Investments | AGF U.S. Large-'
 data = monthly_returns[product_name].to_frame().reset_index()
 data.rename(columns={product_name:'returns'}, inplace = True)
+
+""" 
+  ┌──────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ Create Features                                                                                                  │
+  └──────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
+"""
+
 data['price'] = data['returns'].divide(100).add(1).cumprod() # Create a series of prices starting at $1 based on return series
-
-
-
 data['SMA3'] = data['returns'].rolling(3).mean()
 data['SMA6'] = data['returns'].rolling(6).mean()
 data['SMA12'] = data['returns'].rolling(12).mean()
@@ -58,29 +56,17 @@ data['SMA12'] = data['returns'].rolling(12).mean()
 
 data['rsi'] = RSI(data['price'].to_numpy(), 6)
 
-# ax = sns.distplot(data.rsi.dropna())
-# ax.axvline(30, ls='--', lw=1, c='k')
-# ax.axvline(70, ls='--', lw=1, c='k')
-# ax.set_title('RSI Distribution with Signal Threshold')
-# plt.tight_layout();
-# plt.show()
-
 def compute_bb(close):
     high, mid, low = BBANDS(close, timeperiod=6)
     return pd.DataFrame({'bb_high': high, 'bb_low': low}, index=close.index)
-# fig, axes = plt.subplots(ncols=2, figsize=(15, 5))
-# sns.distplot(data.loc[data.price<100, 'bb_low'].dropna(), ax=axes[0])
-# sns.distplot(data.loc[data.price<100, 'bb_high'].dropna(), ax=axes[1])
-# plt.tight_layout();
-# plt.show()
 data = data.merge(compute_bb(data['price']), left_index=True, right_index = True, how = 'inner')
 # data['bb_high'] = data.bb_high.sub(data.price).div(data.bb_high).apply(np.log1p)
 # data['bb_low'] = data.price.sub(data.bb_low).div(data.price).apply(np.log1p)
 
-# def compute_macd(close):
-#     macd = MACD(close)[0]
-#     return ((macd - np.mean(macd))/np.std(macd)).to_frame().rename(columns = {0: 'macd'})
-# data = data.merge(compute_macd(data['price']), left_index=True, right_index = True, how = 'inner')
+def compute_macd(close):
+    macd = MACD(close, fastperiod=3, slowperiod=6, signalperiod=3)[0] # 12, 26, 9 is default
+    return ((macd - np.mean(macd))/np.std(macd)).to_frame().rename(columns = {0: 'macd'})
+data = data.merge(compute_macd(data['price']), left_index=True, right_index = True, how = 'inner')
 # print(data.macd.describe(percentiles=[.001, .01, .02, .03, .04, .05, .95, .96, .97, .98, .99, .999]).apply(lambda x: f'{x:,.1f}'))
 
 lags = [1, 3, 6, 12]
@@ -111,7 +97,11 @@ for t in [1, 3, 6, 12]:
 # data['month'] = data.index.get_level_values('date').month
 
 
-
+""" 
+  ┌──────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ Model                                                                                                            │
+  └──────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
+"""
 # explore data:
 y = data.filter(like='target').fillna(0)
 X = data.drop(y.columns, axis=1)
@@ -127,7 +117,7 @@ from statsmodels.graphics.tsaplots import plot_acf
 from scipy.stats import norm
 
 target = 'target_1m'
-X = (X.drop(['index', 'returns', 'price'], axis=1)
+X = (X.drop(['index', 'returns', 'price', 'return_1m'], axis=1)
      .transform(lambda x: (x - x.mean()) / x.std())
     .fillna(0))
 print(X)
@@ -136,24 +126,43 @@ print(X.shape)
 model = OLS(endog=y[target], exog=add_constant(X))
 trained_model = model.fit()
 print(trained_model.summary()) 
-
-Xnew = pd.DataFrame([1] + X.iloc[-1].values.tolist()).transpose() 
-ypred = trained_model.predict(Xnew)
-print(ypred)
-# preds = trained_model.predict(add_constant(X))
-# residuals = y[target] - preds
-# fig, axes = plt.subplots(ncols=2, figsize=(14,4))
-# sns.distplot(residuals, fit=norm, ax=axes[0], axlabel='Residuals', label='Residuals')
-# axes[0].set_title('Residual Distribution')
-# axes[0].legend()
-# plot_acf(residuals, lags=10, zero=False, ax=axes[1], title='Residual Autocorrelation')
-# axes[1].set_xlabel('Lags')
-# sns.despine()
-# fig.tight_layout();
+print(trained_model.params)
+# trained_model.params.sort_values().plot.barh( title='Top 10 Coefficients')
 # plt.show()
 
 
+# In Sample prediction
+preds = trained_model.predict(add_constant(X))
+residuals = y[target] - preds
+fig, axes = plt.subplots(ncols=2, figsize=(14,4))
+sns.distplot(residuals, fit=norm, ax=axes[0], axlabel='Residuals', label='Residuals')
+axes[0].set_title('Residual Distribution')
+axes[0].legend()
+plot_acf(residuals, lags=10, zero=False, ax=axes[1], title='Residual Autocorrelation')
+axes[1].set_xlabel('Lags')
+sns.despine()
+fig.tight_layout();
+plt.show()
 
 
+""" 
+  ┌──────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+  │ Measure                                                                                                          │
+  └──────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
+"""
+fig, axes = plt.subplots(ncols=2, figsize=(14,4))
+results = pd.DataFrame()
+results =  data['returns'].shift(-1).to_frame().merge(preds.to_frame(), left_index = True, right_index = True)
+results[0] = results[0].multiply(100)
+print(results)
+from statsmodels.tools.eval_measures import rmse
+rmse = rmse(data['returns'].shift(-1).dropna(), preds[:-1])
+print('RMSE: ', rmse)
 
-# print(data)
+
+# Out of Sample
+# Xnew = pd.DataFrame([1] + X.iloc[-1].values.tolist()).transpose() 
+# ypred = trained_model.predict(Xnew)
+# print(ypred)
+
+
